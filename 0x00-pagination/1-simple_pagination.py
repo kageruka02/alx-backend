b7248a1d7c7d7aeb8dd5a7693e0ebe28611e3975
kageruka02 <leonkageruka@gmail.com>
2024-06-24 16:09:41 +0300

    printing pagess

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -27,8 +27,9 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        assert isinstance(page,int) and page > 0
-        assert isinstance(page_size,int) and page_size > 0
+        "get_page to be printed"
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
         start, end = index_range(page, page_size)
         length = len(self.dataset())
         data = self.dataset()
