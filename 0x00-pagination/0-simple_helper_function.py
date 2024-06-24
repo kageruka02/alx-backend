0f9047279abfff494df39c9a7a36737e51d24734
kageruka02 <leonkageruka@gmail.com>
2024-06-24 14:59:51 +0300

    index_range

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+" return page range"
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple:
+    "return tuple of size 2"
+    return (page_size * page - page_size, page_size * page)
