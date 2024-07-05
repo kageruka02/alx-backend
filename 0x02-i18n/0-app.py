e431b219a1ad6e08c0a9746508483eb5963181e8
kageruka02 <leonkageruka@gmail.com>
2024-07-05 20:09:33 +0300

    flask app

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -9,13 +9,13 @@ from flask import render_template
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
+@app.route("/", strict_slashes=False)
 def index() -> str:
     """
     Renders a basic html template
     """
-    return render_template('0-index.html')
+    return render_template("0-index.html")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
