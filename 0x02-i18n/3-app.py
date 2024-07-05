42c8b094a191a8edfadcd557adb7601c69008e84
kageruka02 <leonkageruka@gmail.com>
2024-07-05 21:31:12 +0300

    flask apps

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,46 +1,27 @@
 #!/usr/bin/env python3
-"""
-A Basic flask application
-"""
-from flask import Flask
-from flask import request
-from flask import render_template
-from flask_babel import Babel
+"3-app.py"
+from flask import Flask, render_template, request
+from flask_babel import Babel, gettext as _
 
-
-class Config(object):
-    """
-    Application configuration class
-    """
-
+class Config:
+    """Configuration class for Babel"""
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
-
-# Instantiate the application object
 app = Flask(__name__)
 app.config.from_object(Config)
-
-# Wrap the application with Babel
 babel = Babel(app)
 
-
 @babel.localeselector
-def get_locale() -> str:
-    """
-    Gets locale from request object
-    """
-    return request.accept_languages.best_match(app.config["LANGUAGES"])
+def get_locale():
+    """Determine the best match with supported languages."""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
+@app.route('/')
+def index():
+    """Route for the home page"""
+    return render_template('3-index.html')
 
-@app.route("/", strict_slashes=False)
-def index() -> str:
-    """
-    Renders a basic html template
-    """
-    return render_template("3-index.html")
-
-
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    app.run(debug=True)
